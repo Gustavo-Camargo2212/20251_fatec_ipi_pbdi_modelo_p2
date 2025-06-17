5d4de021f72dcb4d8c40ce116865c9170786f2a0
Gustavo Camargo <gu-gustavo1999@hotmail.com>
2025-06-17 10:35:01 -0300

    get training and testing bases

--- script.py
+++ script.py
@@ -65,7 +65,7 @@ def codificar_categoricas(features):
 # features de treinamento, features de teste, classe de treinamento, classe de teste
 # a base de treinamento deve ter 75% das instâncias
 def obter_bases_de_treinamento_e_teste(features, classe):
-  pass
+  return train_test_split(features, classe, test_size=0.25, random_state=42)
 
 #REQ 7
 #essa função recebe as features de treinamento e de teste
